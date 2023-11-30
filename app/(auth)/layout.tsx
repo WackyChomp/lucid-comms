77a557ffe0aca09d4be3f72915584d24577a48c0
Wilson Chan <wilsonchan54@gmail.com>
2023-11-29 21:48:32 -0500

    Created route group 'auth' and 'main' / updated gitignore

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+
+const AuthLayout = ({children}: {children: React.ReactNode}) => {
+  return ( 
+    <div className="bg-pink-500 h-full">
+      {children}
+    </div>
+   );
+}
+ 
+export default AuthLayout;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <h1 className='text-green-700 bg-red-300 font-bold'>Hello, welcome to the void</h1>
+      <h1 className='text-green-700 bg-red-300 font-bold'>Hello, welcome to the Main Page</h1>
       <Button variant='default'>Click mwuah</Button>
     </div>
   )

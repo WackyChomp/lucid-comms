47350f00881f26a0f27c45ea4fc9486cc4016e70
Wilson Chan <wilsonchan54@gmail.com>
2024-01-06 22:50:31 -0500

    Added placeholder styling to auth layout.tsx and main page.tsx / added logout button to main page.tsx / updated README.md

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return ( 
-    <div className="bg-pink-500 h-full">
+    <div className="bg-pink-500 h-full items-center-justify-center">
       {children}
     </div>
    );
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <h1 className='text-green-700 bg-red-300 font-bold'>Hello, welcome to the Main Page</h1>
-      <Button variant='default'>Click mwuah</Button>
+    <div className="p-5 flex flex-col">
+      <UserButton afterSignOutUrl="/" />
+
+      <div className="p-5 space-y-5">
+        <h1 className='text-green-700 bg-red-300 font-bold flex justify-center'>Hello, welcome to the Main Page</h1>
+
+        <div className="bg-red-500 flex justify-center space-x-10 p-5">
+          <Button variant='ghost'>Ghostly Button</Button>
+          <Button variant='default'>Regular Button</Button>
+          <Button variant='secondary'>Secondary Button</Button>
+          <Button variant='zhiddentest'>Custom Button</Button>
+        </div>
+      </div>
     </div>
   )
 }
